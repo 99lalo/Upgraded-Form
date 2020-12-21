ce86218fdd59d5e836dc9cc46c097901b0b3405b
99lalo <xx66laloxx@gmail.com>
2020-12-21 22:23:00 +0000

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const send = document.querySelector("#myForm");
+const send = document.querySelector("#forma");
 const cancel = document.querySelector(".btn-secondary");
 const allInputs = document.querySelectorAll(".form-control");
 const errorAlert = ["Card #", "CVC #", "Amount", "First Name", "Last Name", "City", "State", "Postal Code", "Message"];
@@ -10,20 +10,25 @@ const errorAlert = ["Card #", "CVC #", "Amount", "First Name", "Last Name", "Cit
 // const cities = allInputs[5];
 // const state = allInputs[6];
 // const postal = allInputs[7];
-const errorTab = document.querySelector("#error")
-send.addEventListener("submit", (event) => {
+console.log(send);
+
+const errorTab = document.querySelector("#error");
+function Information(event) {
     event.preventDefault();
+    console.log("Im working bro");
     let counter = 0;
     allInputs.forEach(function(inpt, index){
         if(inpt.value){
             counter += 1;
         } else { 
             errorTab.innerHTML += `<div class="alert alert-danger" role="alert"> Unexpected error at the: ${errorAlert[index]} field </div>`
-            inpt.className += `alert-danger`
+            inpt.className += ` alert-danger`
         }
     if(counter == allInputs.length){
         alert(`The form was successfully submitted`)
 
     }
     })
-})
+}
+send.addEventListener("submit", Information);
+
