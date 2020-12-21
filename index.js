988e3b92ce93ed0df14aedc387271f675289590b
99lalo <xx66laloxx@gmail.com>
2020-12-21 21:49:13 +0000

    updates

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const send = document.querySelector("#myForm");
 const cancel = document.querySelector(".btn-secondary");
 const allInputs = document.querySelectorAll(".form-control");
-const card = allInputs[0];
-const cvc = allInputs[1];
-const amount = allInputs[2];
-const fname = allInputs[3];
-const lname = allInputs[4];
-const cities = allInputs[5];
-const state = allInputs[6];
-const postal = allInputs[7];
+const errorAlert = ["Card #", "CVC #", "Amount", "First Name", "Last Name", "City", "State", "Postal Code", "Message"];
+// const card = allInputs[0];
+// const cvc = allInputs[1];
+// const amount = allInputs[2];
+// const fname = allInputs[3];
+// const lname = allInputs[4];
+// const cities = allInputs[5];
+// const state = allInputs[6];
+// const postal = allInputs[7];
 const errorTab = document.querySelector("#error")
-console.log(card.innerHTML.value);
-
 send.addEventListener("submit", (event) => {
     event.preventDefault();
     let counter = 0;
-    let error = 0;
     allInputs.forEach(function(inpt, index){
-        if(inpt.innerHTML.value != undefined){
+        if(inpt.value){
             counter += 1;
-        } else {
-            error = index;
+        } else { 
+            errorTab.innerHTML += `<div class="alert alert-danger" role="alert"> Unexpected error at the: ${errorAlert[index]} field </div>`
+            inpt.className += `alert-danger`
         }
     if(counter == allInputs.length){
-        
+        alert(`The form was successfully submitted`)
+
     }
-        console.log("The form has been submitted");
     })
 })
