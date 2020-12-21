700afee98b12a01a0132eb50f2d922d49de8376a
99lalo <xx66laloxx@gmail.com>
2020-12-21 23:41:58 +0000

    Updates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const errorTab = document.querySelector("#error");
 const usaStates = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 usaStates.forEach((stat,index) => document.querySelector("#inputState").innerHTML += `<option>${usaStates[index]}</option>`)
 
+
 let failure = document.createElement("div");
 errorTab.appendChild(failure)
 
@@ -14,6 +15,7 @@ function Information(event) {
     event.preventDefault();
     let counter = 0;
     let flag = 0;
+    
     allInputs.forEach(function(inpt, index){
         if(inpt.value){
             counter += 1;
@@ -27,11 +29,25 @@ function Information(event) {
             failure.innerHTML += `, ${errorAlert[index]} field`
             inpt.className += ` alert-danger`
         }
-    if(counter == allInputs.length){
+    })
+        if(document.querySelector("#inputState").value != 'Pick a state'){
+        counter += 1;
+    } else {
+            failure.innerHTML += `, ${errorAlert[6]} field`
+            document.querySelector("#inputState").className += ` alert-danger`
+    }
+    if(counter == (allInputs.length +1)){
         failure.innerHTML = `The information was successfully submited`
         failure.className = `alert alert-success`
     }
-    })
 }
 send.addEventListener("submit", Information);
+
+function clear(event) {
+    allInputs.forEach(function(inpt, index){
+        inpt.value = "";
+    })
+    document.querySelector("#inputState").value = 'Pick a state';
+}
+cancel.addEventListener("click", clear)
 
